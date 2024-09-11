43d0c8733070856c8519341e4778a28c1469f8d6
JS LEE <109492059+JSLEE753@users.noreply.github.com>
2024-09-12 05:36:08 +0900

    fix: css 오류 수정
    Header, Sidebar, Router 의 height 조정

--- src/components/sidebar/Sidebar.style.ts
+++ src/components/sidebar/Sidebar.style.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 export const SidebarStyle = styled.div`
-  /* background-color: ${(props) => props.theme.color.main}; */
   background-color: ${({ theme }) => theme.color.main};
   min-width: 300px;
-  height: 100vh;
+  min-height: 100vh;
 `;
