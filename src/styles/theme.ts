9f9c5b64c35ad7ac13d505a1a4f0d183d01fe47a
NASUNJIN <sj010618@gmail.com>
2024-09-14 17:25:51 +0900

    feat: studyRoomButton, borderRadius 테마 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,21 @@
-import { DefaultTheme } from 'styled-components';
+// import { DefaultTheme } from 'styled-components';
 
-export interface Colors {
-  main: string;
-  mainStrong: string;
-  lineGray: string;
-  btnOk: string;
-  btnWarn: string;
-  bgGray: string;
-  bgLightGray: string;
-  bgYellowNote: string;
-}
+export type ColorKey = "main" | "mainStrong" | "lineGray" | "btnOk" | "btnWarn" | "bgGray" | "bgLightGray" | "bgYellowNote";
+export type ButtonSize = "large" | "medium" | "small";
+export type BorderRadius = "large" | "medium" | "small";
+
+interface DefaultTheme {
+  color: Record<ColorKey, string>;
+  studyRoomButton: {
+    [key in ButtonSize] : {
+      width: string;
+      height: string;
+    };
+  };
+  borderRadius: {
+    [key in BorderRadius]: string;
+  };
+};
 
 export const theme: DefaultTheme = {
   color: {
@@ -21,5 +27,24 @@ export const theme: DefaultTheme = {
     bgGray: '#E5E5E5',
     bgLightGray: '#F9F9F9',
     bgYellowNote: '#FFF9EE',
+  },
+  studyRoomButton: {
+    large: {
+      width: "439px",
+      height: "77px",
+    },
+    medium: {
+      width: "328px",
+      height: "68px",
+    },
+    small: {
+      width: "262px",
+      height: "92px",
+    },
+  },
+  borderRadius: {
+    large: "15px",
+    medium: "10px",
+    small: "5px",
   },
 };
