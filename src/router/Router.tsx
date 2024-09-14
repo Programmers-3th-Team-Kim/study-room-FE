e789741f1d29249f24448ba9fe16c1b2b9f8db8c
bang-wol <milk011922@gmail.com>
2024-09-14 14:32:11 +0900

    feat: 로그인, 회원가입 라우터 설정

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,14 +1,25 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Sidebar from '../components/sidebar/Sidebar';
 import { MainContentArea, RouterStyle } from './Router.style';
+import LoginPage from '../pages/login/LoginPage';
+import RegisterPage from '../pages/register/RegisterPage';
 
 export default function Router() {
+  const location = useLocation();
+
+  const isAuthPage =
+    location.pathname === '/login' || location.pathname === '/register';
+
   return (
     <RouterStyle>
-      <Sidebar />
+      {!isAuthPage && <Sidebar />}
       <MainContentArea>
-        <Header />
-        <div>컨텐츠 영역</div>
+        {!isAuthPage && <Header />}
+        <Routes>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+        </Routes>
       </MainContentArea>
     </RouterStyle>
   );
