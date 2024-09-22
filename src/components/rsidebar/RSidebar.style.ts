5afcc8452a733c10fa5ac7fd2e30c5301b1757dd
JS LEE <109492059+JSLEE753@users.noreply.github.com>
2024-09-23 03:37:23 +0900

    feat: 스터디룸 RSideBar 할일 구현
    * RSideBar UI 구현
    * RSideBar-할 일 UI 구현
    * 임시API연동
    
    예정 ) RSIdeBar 채팅 구현

--- src/components/rsidebar/RSidebar.style.ts
+++ src/components/rsidebar/RSidebar.style.ts
@@ -1,8 +1,60 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const RSidebarStyle = styled.div`
   min-width: 600px;
   height: 100vh;
-  float: right;
   background: ${({ theme }) => theme.color.main};
+  display: flex;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100%;
+`;
+
+export const CurrentTime = styled.div`
+  color: #7c7c7c; // plannerTimeGray
+  font-size: 36px;
+  font-weight: 600;
+`;
+
+export const ContentWrapper = styled.div`
+  width: 494px;
+  height: 750px;
+  border-radius: 15px;
+  background-color: white;
+  margin: 8px 0px;
+`;
+
+export const TabsWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Tab = styled.div.withConfig({
+  shouldForwardProp: (prop) => !['isSelected'].includes(prop),
+})<{ isSelected?: boolean }>`
+  font-size: 36px;
+
+  color: ${(props) => (props.isSelected ? 'black' : '#7c7c7c')};
+  text-align: center;
+  width: 120px;
+  border-radius: 8px;
+  padding: 10px;
+  background-color: white;
+  /* border: 1px solid #7c7c7c; */
+
+  &:hover {
+    cursor: pointer;
+    background-color: #e6e6e6;
+  }
+
+  ${(props) =>
+    props.isSelected && `background-color : ${props.theme.color.bgGray}`}
 `;
