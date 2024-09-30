9a14adcec4449a28f150b31b490f5b46bb4b1e3f
bang-wol <milk011922@gmail.com>
2024-09-30 10:25:23 +0900

    refactor: 버튼 감싸고 있는 div 삭제

--- src/pages/study-room/components/button/CreateButton.style.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const CreateButtonStyle = styled.div`
-  display: flex;
-  margin-left: auto;
-  margin-right: 29px;
-`;
--- src/pages/study-room/components/button/CreateButton.tsx
+++ src/pages/study-room/components/button/CreateButton.tsx
@@ -1,19 +1,21 @@
 import SCreateButton from '@/components/button/SCreateButton';
 import { FaPlus } from 'react-icons/fa';
-import * as S from './CreateButton.style';
 
-function CreateButton() {
+interface CreateButtonProps {
+  onClick: () => void;
+}
+
+function CreateButton({ onClick }: CreateButtonProps) {
   return (
-    <S.CreateButtonStyle>
-      <SCreateButton
-        label="스터디방 개설"
-        size="medium"
-        Icon={FaPlus}
-        borderRadius="10px"
-        fontSize="24px"
-        iconSize="32px"
-      />
-    </S.CreateButtonStyle>
+    <SCreateButton
+      label="스터디방 개설"
+      size="medium"
+      Icon={FaPlus}
+      borderRadius="10px"
+      fontSize="24px"
+      iconSize="32px"
+      onClick={onClick}
+    />
   );
 }
 
