f892bcbf26809c255076e0b75d3aaf4805b0ae35
bang-wol <milk011922@gmail.com>
2024-10-02 18:14:26 +0900

    refactor: cookie 사용, request 인터셉터 추가

--- src/apis/axiosInstance.api.ts
+++ src/apis/axiosInstance.api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '@/stores/auth.store';
 import { API_ROUTES } from './apiRoutes';
+import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_REACT_APP_API_URL}`,
@@ -13,10 +14,6 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    if (originalRequest.url.includes(API_ROUTES.LOGIN)) {
-      return Promise.reject(error);
-    }
-
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
@@ -27,18 +24,16 @@ axiosInstance.interceptors.response.use(
         const newAccessToken = refreshResponse.data.accessToken;
         const user = refreshResponse.data.user;
 
-        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-
         const { setAuthData } = useAuthStore.getState();
         setAuthData(newAccessToken, user);
+        Cookies.set('accessToken', newAccessToken, { expires: 1 });
 
+        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
         return axiosInstance(originalRequest);
       } catch (refreshError) {
         console.error('토큰 갱신 실패', refreshError);
-
         const { clearAuthData } = useAuthStore.getState();
         clearAuthData();
-
         return Promise.reject(refreshError);
       }
     }
@@ -46,5 +41,13 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+axiosInstance.interceptors.request.use((config) => {
+  const { accessToken } = useAuthStore.getState();
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
 
 export default axiosInstance;
