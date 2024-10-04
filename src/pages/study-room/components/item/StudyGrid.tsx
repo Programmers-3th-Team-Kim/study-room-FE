ddfafaad1c09a6521e8423ac35e0bf358444561c
NASUNJIN <sj010618@gmail.com>
2024-10-04 20:03:07 +0900

    feat: TanStack Query v5 useInfiniteQuery 부분

--- src/pages/study-room/components/item/StudyGrid.tsx
+++ src/pages/study-room/components/item/StudyGrid.tsx
@@ -1,29 +1,35 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import StudyItem from './StudyItem';
 import * as S from './StudyGrid.style';
 import axios from 'axios';
 import { FetchRoomsParams, Room } from '@/types/studyRoom';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Modal from '@/components/modal/Modal';
 import PasswordInput from '../form/PasswordInput';
 import axiosInstance from '@/apis/axiosInstance.api';
 
 // API 요청 함수
-const fetchRooms = async (params: FetchRoomsParams) => {
-  const { search, isPublic, isPossible, limit, offset } = params;
-
+const fetchRooms = async ({
+  queryKey,
+}: {
+  queryKey: [string, FetchRoomsParams];
+}): Promise<Room[]> => {
+  const [, params] = queryKey;
   const query = new URLSearchParams({
-    search: search || '',
-    isPublic: isPublic !== undefined ? String(isPublic) : '',
-    isPossible: isPossible !== undefined ? String(isPossible) : '',
-    limit: limit !== undefined ? String(limit) : '',
-    offset: offset !== undefined ? String(offset) : '',
+    search: params.search || '',
+    isPublic: params.isPublic !== undefined ? String(params.isPublic) : '',
+    isPossible:
+      params.isPossible !== undefined ? String(params.isPossible) : '',
+    limit: String(params.limit),
+    offset: String(params.limit),
   }).toString();
 
   const url = `${import.meta.env.VITE_REACT_APP_API_URL}/rooms?${query}`;
-  console.log('Fetching URL:', url);
   const res = await axios.get(url);
+
+  console.log('Fetching URL:', url);
+
   return res.data;
 };
 
@@ -32,7 +38,6 @@ const checkPassword = async (roomId: string, password: string) => {
     `${import.meta.env.VITE_REACT_APP_API_URL}/rooms/checkPassword/${roomId}`,
     { password }
   );
-  console.log(response.data);
   return response.data;
 };
 
@@ -46,25 +51,35 @@ function StudyGrid({
   const [selectedRoom, setSelectedRoom] = useState<Room | null>(null);
   const [password, setPassword] = useState('');
 
+  const initialLimit = 4;
+  const [limit, setLimit] = useState(initialLimit);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  //console.log('previousScrollHeight: ', previousScrollHeight);
+
   const params: FetchRoomsParams = {
     search: filter.search || '',
     isPublic: filter.isPublic,
     isPossible: filter.isPossible,
-    limit: 40,
-    offset: 0, // 예시로 0으로 설정
+    limit,
   };
 
   const {
-    data: rooms = [],
+    data: roomsData,
+    fetchNextPage,
+    hasNextPage,
     isLoading,
     error,
-  } = useQuery<Room[], Error>({
+  } = useInfiniteQuery<Room[], Error>({
     queryKey: ['rooms', params],
-    queryFn: () => fetchRooms(params),
+    queryFn: fetchRooms,
+    getNextPageParam: (lastPage, pages) =>
+      lastPage.length === limit ? pages.length * limit : undefined,
+    staleTime: 1000 * 60 * 1, // 1분 동안 신선한 데이터
+    cashTime: 1000 * 60 * 2, // 2분 동안 캐시 유지
   });
 
   const handleRoomClick = (room: Room) => {
-    //console.log('선택한 방', room);
     if (room.isPublic) {
       navigate(`/study-room/${room._id}`);
     } else {
@@ -73,9 +88,6 @@ function StudyGrid({
     }
   };
 
-  // 무한 스크롤 구현 필요 (스크롤이 바닥에 닿았을 경우, 특정 요소가 얼마만큼 보였는지)
-
-  // 비밀번호 일치할 경우 : 비밀번호 확인 완료 message 받음
   const handlePasswordSubmit = async () => {
     if (selectedRoom) {
       try {
@@ -86,15 +98,109 @@ function StudyGrid({
           alert('비밀번호가 일치하지 않습니다.');
         }
       } catch (error) {
-        console.error('비밀번호 확인 중 오류 발생:', error);
         alert('비밀번호 확인 중 오류가 발생했습니다.');
+      } finally {
+        setShowPasswordModal(false);
       }
-      setShowPasswordModal(false);
     }
   };
 
+  /*
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const { scrollHeight, scrollTop, clientHeight } = e.currentTarget;
+    const bottom = scrollHeight - scrollTop <= clientHeight + 50;
+
+    // 스크롤 위치 저장
+    previousScrollHeight.current = scrollHeight;
+
+    console.log('previousScrollHeightCurrent', previousScrollHeight.current);
+
+    if (bottom && hasNextPage) {
+      console.log('bottom && hasNextPage');
+      const currentScrollTop = scrollTop;
+      // console.log(currentScrollTop);
+      setLimit((prevLimit) => prevLimit + initialLimit);
+      fetchNextPage().then(() => {
+        const newScrollHeight = containerRef.current?.scrollHeight || 0;
+        const scrollOffset = newScrollHeight - previousScrollHeight.current;
+
+        containerRef.current?.scrollTo({
+          top: currentScrollTop + scrollOffset,
+          behavior: 'smooth',
+        });
+
+        console.log(
+          'previousScrollHeight.current',
+          previousScrollHeight.current
+        );
+        console.log('containerRef.current', containerRef.current);
+        console.log('newScrollHeight', newScrollHeight);
+        console.log('currentScrollTop', currentScrollTop);
+        console.log('scrollOffset', scrollOffset);
+        console.log('스크롤 위치', currentScrollTop + scrollOffset);
+        console.log(
+          'previousScrollHeight.current',
+          previousScrollHeight.current
+        );
+      });
+    }
+  };
+  */
+
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const { scrollHeight, scrollTop, clientHeight } = e.currentTarget;
+    const bottom = scrollHeight - scrollTop <= clientHeight;
+
+    // 스크롤 위치 저장
+    const prevScrollTop = scrollTop;
+
+    console.log(
+      'scrollHeight, scrollTop, clientHeight',
+      scrollHeight,
+      scrollTop,
+      clientHeight
+    );
+
+    console.log('prevScrollTop', prevScrollTop);
+
+    if (bottom && hasNextPage) {
+      console.log('bottom && hasNextPage');
+
+      const currentScrollTop = scrollTop;
+
+      console.log('prevScrollTop, scrollTop', prevScrollTop, scrollTop);
+      console.log('setLimit 전');
+      setLimit((prevLimit) => prevLimit + initialLimit);
+      console.log('setLimit 후');
+      console.log('prevScrollTop, scrollTop', prevScrollTop, scrollTop);
+      console.log('currentScrollTop', currentScrollTop);
+      fetchNextPage()
+        .then(() => {
+          console.log('fetchNextPage 성공');
+          //const newScrollTop = prevScrollTop;
+          //console.log('newScrollTop 1', newScrollTop);
+
+          containerRef.current?.scrollTo({
+            top: currentScrollTop,
+            behavior: 'smooth',
+          });
+
+          //const newScrollTop = prevScrollTop;
+          console.log('fetchNextPage 안 prevScrollTop', prevScrollTop);
+          //console.log('newScrollTop 2', newScrollTop);
+          console.log('currentScrollTop', currentScrollTop);
+        })
+        .catch((error) => {
+          console.error('fetchNextPage 실패', error);
+        });
+      console.log('fetchNextPage 밖 : prevScrollTop', prevScrollTop);
+    }
+  };
+
+  const rooms = roomsData?.pages.flat() || [];
+
   return (
-    <S.StudyGridStyle>
+    <S.StudyGridStyle ref={containerRef} onScroll={handleScroll}>
       {isLoading ? (
         <div>로딩 중...</div>
       ) : error ? (
@@ -103,7 +209,6 @@ function StudyGrid({
         rooms.map((room) => (
           <div key={room._id} onClick={() => handleRoomClick(room)}>
             <StudyItem
-              key={room._id}
               title={room.title}
               imageUrl={room.imageUrl}
               tagList={room.tagList}
