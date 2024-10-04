b8911fbcdb3972fe13d74a3440e66c5372f2c797
bang-wol <milk011922@gmail.com>
2024-10-04 11:14:02 +0900

    fix: apiRoutes 경로 수정

--- src/apis/apiRoutes.ts
+++ src/apis/apiRoutes.ts
@@ -4,7 +4,7 @@ export const API_ROUTES = {
   SIGNUP: '/auth/signup',
   REFRESH_TOKEN: '/auth/refresh-token',
   DUPLICATE: '/auth/check-duplicate',
-  VERIFY_PASSWORD: '/auth/change-password',
+  VERIFY_PASSWORD: '/auth/verify-password',
   CHANGE_PASSWORD: '/auth/change-password',
   USER: '/users/me',
 };
