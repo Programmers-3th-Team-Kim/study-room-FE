8155a0c8a7f6495b7cbf8aa2c4ea7e1a8e0c67ec
bang-wol <milk011922@gmail.com>
2024-10-11 18:26:23 +0900

    refactor: 전체 통계 평균 데이터 중복 코드 줄이기

--- src/pages/statistics/all/AllStatisticsPage.tsx
+++ src/pages/statistics/all/AllStatisticsPage.tsx
@@ -4,6 +4,48 @@ import { fetchAllAverage } from '@/apis/statistics.api';
 import { formatAverageTime } from '@/utils/formatTime';
 import MyLineChart from './components/myLineChart/MyLineChart';
 
+interface AverageSection {
+  title: string;
+  average: {
+    yesterday: { hours: string; minutes: string };
+    lastWeek: { hours: string; minutes: string };
+    lastMonth: { hours: string; minutes: string };
+  };
+}
+
+const AverageSection = ({ title, average }: AverageSection) => (
+  <S.Avg>
+    <S.AvgTitle>{title}</S.AvgTitle>
+    <div>
+      <S.AvgLine>
+        전날 평균
+        <S.AvgSpan>
+          {formatAverageTime(
+            average.yesterday.hours,
+            average.yesterday.minutes
+          )}
+        </S.AvgSpan>
+      </S.AvgLine>
+      <S.AvgLine>
+        저번 주 평균
+        <S.AvgSpan>
+          {formatAverageTime(average.lastWeek.hours, average.lastWeek.minutes)}
+        </S.AvgSpan>
+      </S.AvgLine>
+      <S.AvgLine>
+        저번 달 평균
+        <S.AvgSpan>
+          {formatAverageTime(
+            average.lastMonth.hours,
+            average.lastMonth.minutes
+          )}
+        </S.AvgSpan>
+        공부했어요!
+      </S.AvgLine>
+    </div>
+  </S.Avg>
+);
+
 export default function AllStatisticsPage() {
   const [averageData, setAverageData] = useState({
     all: {
@@ -35,75 +77,8 @@ export default function AllStatisticsPage() {
     <S.StatContainer>
       <div>
         <S.AvgWrapper>
-          <S.Avg>
-            <S.AvgTitle>
-              전체 <br />
-              사용자는
-            </S.AvgTitle>
-            <div>
-              <S.AvgLine>
-                전날 평균
-                <S.AvgSpan>
-                  {formatAverageTime(
-                    averageData.all.yesterday.hours,
-                    averageData.all.yesterday.minutes
-                  )}
-                </S.AvgSpan>
-              </S.AvgLine>
-              <S.AvgLine>
-                저번 주 평균
-                <S.AvgSpan>
-                  {formatAverageTime(
-                    averageData.all.lastWeek.hours,
-                    averageData.all.lastWeek.minutes
-                  )}
-                </S.AvgSpan>
-              </S.AvgLine>
-              <S.AvgLine>
-                저번 달 평균
-                <S.AvgSpan>
-                  {formatAverageTime(
-                    averageData.all.lastMonth.hours,
-                    averageData.all.lastMonth.minutes
-                  )}
-                </S.AvgSpan>
-                공부했어요!
-              </S.AvgLine>
-            </div>
-          </S.Avg>
-          <S.Avg>
-            <S.AvgTitle>닉네임님은</S.AvgTitle>
-            <div>
-              <S.AvgLine>
-                전날 평균
-                <S.AvgSpan>
-                  {formatAverageTime(
-                    averageData.my.yesterday.hours,
-                    averageData.my.yesterday.minutes
-                  )}
-                </S.AvgSpan>
-              </S.AvgLine>
-              <S.AvgLine>
-                저번 주 평균
-                <S.AvgSpan>
-                  {formatAverageTime(
-                    averageData.my.lastWeek.hours,
-                    averageData.my.lastWeek.minutes
-                  )}
-                </S.AvgSpan>
-              </S.AvgLine>
-              <S.AvgLine>
-                저번 달 평균
-                <S.AvgSpan>
-                  {formatAverageTime(
-                    averageData.my.lastMonth.hours,
-                    averageData.my.lastMonth.minutes
-                  )}
-                </S.AvgSpan>
-                공부했어요!
-              </S.AvgLine>
-            </div>
-          </S.Avg>
+          <AverageSection title="전체 사용자는" average={averageData.all} />
+          <AverageSection title="닉네임님은" average={averageData.my} />
         </S.AvgWrapper>
       </div>
       <MyLineChart />
